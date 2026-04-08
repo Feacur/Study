dd9b37ed0b5b7280365d98322d7726556433b603
feacur <feacur@gmail.com>
2026-04-08 11:57:15 +0200

    prettify systems a bit
    - services can be disposed
    - deconstruct kvps
    - load by type
    - reset specifically only editor

--- Assets/StudyPhotonBare/Code/EntryPoint.cs
+++ Assets/StudyPhotonBare/Code/EntryPoint.cs
@@ -13,23 +13,31 @@ public static class EntryPoint
 	[RuntimeInitializeOnLoadMethod]
 	private static void RuntimeInitializeOnLoad()
 	{
-		Application.quitting += OnQuit;
 		QualitySettings.vSyncCount = 0;
 		Application.targetFrameRate = 60;
 
-		PoolOfGameObjects.Init();
-
 		ServiceLocator.Set(new ResourcesService());
 		ServiceLocator.Set(new NetworkService());
+
+		PoolOfGameObjects.Init();
+		// @todo init via the `EventBus` ?
+		// is not appropriate for statics; but they can be singletons then (or services?)
+
+	#if UNITY_EDITOR // cleanup everything for
+		UnityEditor.EditorApplication.playModeStateChanged += PlayModeStateChanged;
+		static void PlayModeStateChanged(UnityEditor.PlayModeStateChange state)
+		{
+			if (state != UnityEditor.PlayModeStateChange.ExitingPlayMode) return;
+			UnityEditor.EditorApplication.playModeStateChanged -= PlayModeStateChanged;
+			// @todo reset via the `EventBus` ?
+			// then make sure it's the last and doesn't pool anything
+			EventBus.Reset();
+			ServiceLocator.Reset();
+			PoolOfGameObjects.Reset();
+		}
+	#endif
 	}
 
-	private static void OnQuit()
-	{
-		Application.quitting -= OnQuit;
-		PoolOfGameObjects.Reset();
-		ServiceLocator.Reset();
-		EventBus.Reset();
-	}
 }
 
 }
--- Assets/StudyPhotonBare/Code/Tools/ServiceLocator.cs
+++ Assets/StudyPhotonBare/Code/Tools/ServiceLocator.cs
@@ -11,10 +11,13 @@ public static class ServiceLocator
 	// @todo store the very basic type instead
 	// - getting a value of a static field with name "ServiceType" is a possibility
 	// - no chance using static interface properties with the current C# version
+	// - just fetch interfaces like with the `EventBus` ?
 	private static readonly Dictionary<Type, IService> _instances = new Dictionary<Type, IService>();
 
 	public static void Reset()
 	{
+		foreach (var (_, instance) in _instances)
+			DisposeService(instance);
 		_instances.Clear();
 	}
 
@@ -28,7 +31,15 @@ public static class ServiceLocator
 	public static void Set<T>(T instance) where T : IService
 	{
 		var type = typeof(T);
+		if (_instances.TryGetValue(type, out var existing))
+			DisposeService(existing);
 		_instances[type] = instance;
+	}
+
+	private static void DisposeService(IService instance)
+	{
+		if (instance is IDisposable disposable)
+			disposable.Dispose();
 	}
 }
 
